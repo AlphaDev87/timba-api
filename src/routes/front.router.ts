aacfd158798280fbacd44e877df70c58c3e880e5
Rodrigo Alvarez <alvarezrrj@gmail.com>
2024-10-28 16:32:53 -0300

    Stop serving frontend assets

--- src/routes/front.router.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-const frontRouter = Router();
-
-frontRouter.get("/", (_req: Req, res: Res, _next: NextFn) =>
-  res.status(301).setHeader("Location", "https://casino-mex.com").send(),
-);
-
-export default frontRouter;
