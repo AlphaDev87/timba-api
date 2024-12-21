7506efbc83ed01eb7507db41941ae9aa1118fba6
Dasit <foxdev@Dasits-MacBook-Pro.local>
2024-12-20 22:35:10 -0300

    fix reset migrations

--- prisma/migrations/20241216170438_make_all_fields_optional_on_deposit/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `DEPOSITS` MODIFY `tracking_number` VARCHAR(191) NULL,
-    MODIFY `amount` DOUBLE NULL,
-    MODIFY `date` DATETIME(3) NULL,
-    MODIFY `sending_bank` VARCHAR(191) NULL;
