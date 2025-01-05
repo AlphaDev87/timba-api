c905139ab1f383c9b4963d69e19283acafad7fe0
AlphaDev87 <155021991+AlphaDev87@users.noreply.github.com>
2025-01-05 03:24:32 -0300

    Feat/sse (#111) (#112) (#110)
    * Feat/sse (#111)
    
    * Patch bonus flow
    
    * Bump minor version
    
    * Fix player phone number validator
    
    * Revert back to `"module": "CommonJs"`
    
    * Clean up
    
    * Fix table name && bump minor version
    
    * Add unauthenticated SSE
    
    * Add /auth/sse-token to retreive SSE access token
    
    * Update short-lived token duration
    
    * Add authentication to /transactions/deposit/sse
    
    * 🧹 Clean up
    
    * Fix authRouter
    
    ---------
    
    
    
    * fixes/cointransfer-status
    
    ---------
    
    Co-authored-by: Rodrigo Alvarez <alvarezrrj@gmail.com>
    Co-authored-by: Dasit <foxdev@Dasits-MacBook-Pro.local>

--- src/components/deposits/services.ts
+++ src/components/deposits/services.ts
@@ -189,7 +189,12 @@ export class DepositServices extends ResourceService {
   private dispatchSSE(deposit: Deposit) {
     const { DEPOSIT_EVENT, eventTarget } = DepositSSE;
     const customEvent = new CustomEvent(DEPOSIT_EVENT, {
-      detail: { [deposit.id]: deposit.status, userId: deposit.player_id },
+      detail: {
+        eventType: DEPOSIT_EVENT,
+        depositId: deposit.id,
+        status: deposit.status,
+        userId: deposit.player_id,
+      },
     });
     eventTarget.dispatchEvent(customEvent);
   }
