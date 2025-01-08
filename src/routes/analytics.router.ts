a6cf7bff9a5dafd97f53efe38b25d9b29138bccc
Dasit <foxdev@Dasits-MacBook-Pro.local>
2025-01-08 12:44:17 -0300

    fix/analytics

--- src/routes/analytics.router.ts
+++ src/routes/analytics.router.ts
@@ -3,13 +3,13 @@ import { checkExact } from "express-validator";
 import passport from "passport";
 import { AnalyticsController } from "@/components/analytics/controller";
 import { throwIfBadRequest } from "@/middlewares/requestErrorHandler";
+import { requireAgentRole } from "@/middlewares/auth";
 import {
   isKeyOfAnalytics,
   validateAnalyticsRequest,
   validateId,
 } from "@/components/analytics/validators";
 import { validateResourceSearchRequest } from "@/components/analytics/validators";
-import { requireAgentRole } from "@/middlewares/auth";
 
 const analyticsRouter = Router();
 
@@ -22,11 +22,6 @@ analyticsRouter.get(
   throwIfBadRequest,
   AnalyticsController.index,
 );
-analyticsRouter.use(
-  passport.authenticate("jwt", { session: false, failWithError: true }),
-);
-analyticsRouter.use(requireAgentRole);
-analyticsRouter.post("/summary", AnalyticsController.summary);
 analyticsRouter.get(
   "/:id",
   validateId(),
@@ -41,5 +36,10 @@ analyticsRouter.post(
   throwIfBadRequest,
   AnalyticsController.create,
 );
+analyticsRouter.use(
+  passport.authenticate("jwt", { session: false, failWithError: true }),
+);
+analyticsRouter.use(requireAgentRole);
+analyticsRouter.post("/summary", AnalyticsController.summary);
 
 export default analyticsRouter;
